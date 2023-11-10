b6042ae3bbfa26fe1cd9d7ae4fe76b541a4e694e
Tomasz Koralewski <tomasz2k02@gmail.com>
2023-11-10 22:57:26 +0100

    CityChallange fixes

---
+++ backend/src/main/java/com/example/backend/cityChallenge/CityChallengeConfig.java
@@ -0,0 +1,20 @@
+package com.example.backend.cityChallenge;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class CityChallengeConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunnerCityChallenge(CityChallengeRepository repository){
+        return args -> {
+            CityChallenge cityChallenge1 = new CityChallenge("Snake", "Big snake is snaking","Johnny Bravo");
+            CityChallenge cityChallenge2 = new CityChallenge("Tetris", "Big blocks are falling","Geralt of Rivia");
+            repository.saveAll(List.of(cityChallenge1, cityChallenge2));
+        };
+    }
+}
