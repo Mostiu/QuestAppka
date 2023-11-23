bd110f1d8940c79ae5ad72869b7ac87cd25c40bf
Sianuga <dzikiewicz.pawel@gmail.com>
2023-11-24 00:05:54 +0100

    Cards added

--- frontend/src/Routes/HomePage.js
+++ frontend/src/Routes/HomePage.js
@@ -2,22 +2,45 @@ import React from 'react';
 import '../Styles/Home.css';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import axios from "axios";
 import InfiniteScroll from 'react-infinite-scroll-component';
+import Card from "../Components/Card";
 
-// requires a loader
 class HomePage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            cityChallenges: []
-        }
+            cityChallenges: [],
+            randomWords: [],
+            hasMore: true,
+            page: 1
+        };
     }
 
+    componentDidMount() {
+        this.fetchCityChallenges();
+        this.fetchRandomWords();
+    }
+
+    fetchCityChallenges = () => {
+        // Use a real API call for city challenges
+        // Assuming an endpoint like 'http://localhost:8080/api/cityChallenges'
+        fetch('http://localhost:8080/api/cityChallenges')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ cityChallenges: data });
+            })
+            .catch(error => {
+                console.error('Error fetching city challenges:', error);
+            });
+    }
 
     fetchRandomWords = () => {
         // Mock API call for random words
         const mockRandomWords = [
+            'Apple', 'Banana', 'Carrot', 'Dolphin', 'Elephant',
+            'Flower', 'Giraffe', 'Happiness', 'Igloo', 'Jazz',
+            'Apple', 'Banana', 'Carrot', 'Dolphin', 'Elephant',
+            'Flower', 'Giraffe', 'Happiness', 'Igloo', 'Jazz',
             'Apple', 'Banana', 'Carrot', 'Dolphin', 'Elephant',
             'Flower', 'Giraffe', 'Happiness', 'Igloo', 'Jazz',
             // Add more words as needed
@@ -36,27 +59,16 @@ class HomePage extends React.Component {
         }));
     }
 
-    componentDidMount() {
-        axios.get('http://localhost:8080/api/cityChallenges')
-            .then(response => {
-                this.setState({ cityChallenges: response.data })
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }
-
     render() {
-        console.log("City Challenges:", this.state.cityChallenges[0]);
+        const { randomWords } = this.state;
 
         return (
             <div className="Home">
                 <div className={"LeftContainer"}>
                     <div className={"CityChallenge"}>
                         <Carousel infiniteLoop={true}
-                            showIndicators={false}
-                            showStatus={false}>
-
+                                  showIndicators={false}
+                                  showStatus={false}>
                             {this.state.cityChallenges.map(challenge => (
                                 <div key={challenge.id}>
                                     <h2>{challenge.title}</h2>
@@ -66,28 +78,39 @@ class HomePage extends React.Component {
                         </Carousel>
                     </div>
 
-                    <h1 className={"CoursesStartedH1"}>Home Page</h1>
 
+                    <div className={"RightContainer"} style={{ height: '250px', overflowY: 'scroll', paddingLeft: '40px' }}>
+                        <InfiniteScroll
+                            dataLength={randomWords ? randomWords.length : 0}
+                            next={this.fetchMoreData}
+                            hasMore={this.state.hasMore}
+                            loader={<h4>Loading...</h4>}
+                            endMessage={<p>No more words to show.</p>}
+                            style={{ padding: '0 20px' }}
+                        >
+                            {randomWords && randomWords.map((word, index) => (
+                                <Card></Card>
+                            ))}
+                        </InfiniteScroll>
+                    </div>
                 </div>
 
-               <div className={"RightContainer"}>
-                   <InfiniteScroll
-                       dataLength={this.state.randomWords.length}
-                       next={this.fetchMoreData}
-                       hasMore={this.state.hasMore}
-                       loader={<h4>Loading...</h4>}
-                       endMessage={<p>No more words to show.</p>}
-                   >
-                       {this.state.randomWords.map((word, index) => (
-                           <div key={index}>
-                               <h1>{word}</h1>
-                           </div>
-                       ))}
-                   </InfiniteScroll>
-
-               </div>
-
-
+                <div className={"RightContainer"} style={{ height: '480px', overflowY: 'scroll' }}>
+                    <InfiniteScroll
+                        dataLength={randomWords ? randomWords.length : 0}
+                        next={this.fetchMoreData}
+                        hasMore={this.state.hasMore}
+                        loader={<h4>Loading...</h4>}
+                        endMessage={<p>No more words to show.</p>}
+                        style={{ padding: '0 20px' }}
+                    >
+                        {randomWords && randomWords.map((word, index) => (
+                            <div key={index}>
+                                <h1>{word}</h1>
+                            </div>
+                        ))}
+                    </InfiniteScroll>
+                </div>
             </div>
         );
     }
